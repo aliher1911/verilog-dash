2e7e7ee023f42bf7eb32f02670e7cbb87de20271
Oleg Afanasyev <olegaf@fb.com>
2018-02-12 10:28:03 +0000

    Update docs with table styles

--- tasks/generator_tb.v
+++ tasks/generator_tb.v
@@ -18,14 +18,12 @@ module top;
 
    reg [4095:0] vcdfile;
 
-   assign rnd = $random;
-   
    initial begin
       if ($value$plusargs("waveform=%s", vcdfile)) begin
 	 $dumpfile(vcdfile);
 	 $dumpvars(0, top);
       end
-      $monitor("Random @%t/%d = %d", $time, clk, rnd);
+      $monitor("UUT @%t/%d, res %d, en %d, %h", $time, clk, res, en, result);
 
       @(posedge clk);
       res <= 1;
@@ -37,6 +35,16 @@ module top;
       en <= 1;
       repeat (5) @(posedge clk);
       en <= 0;
+
+      @(posedge clk);
+
+      $display("Deposit1 %d, %d", res, en, $time);
+      $strobe("Deposits %d, %d", res, en, $time);
+
+      $deposit(uut.en, 1);
+      $display("Deposit2 %d, %d", res, en, $time);
+      res <= 1'bz;
+      $display("Deposit3 %d, %d", res, en, $time);      
       repeat (5) @(posedge clk);      
       
       $display("Before finish at %t", $time);
